1f9a640e5143d4a5168ed8e7c18cae2854864f06
Gwenstaychee <145661822+Gwenstaychee@users.noreply.github.com>
2023-10-29 22:32:01 +0700

    Add files via upload

---
+++ lecture17/exercise1/components/Sidebar.js
@@ -0,0 +1,11 @@
+class SidebarPage extends React.Component {
+    render() {
+        return(
+            <u1>
+                <Ii><a href="#">London</a></Ii>
+                <Ii><a href="#">Paris</a></Ii>
+                <Ii><a href="#">Tokyo</a></Ii>
+            </u1>
+        );
+    }
+}
